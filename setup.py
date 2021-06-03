ef5a17a950fbe46604f54a504491e7e6f30f0813
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2021-06-03 15:01:55 +0200

    year

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "righteous-fa",
-    version = "1.2.1",
+    version = "1.3.0",
     author = "Richard Tjörnhammar",
     author_email = "richard.tjornhammar@gmail.com",
     description = "Righteous Pathway Factor Analysis",
